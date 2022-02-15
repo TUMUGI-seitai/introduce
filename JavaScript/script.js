638c712891a718074d02f43374f5ba2e169b7627
TUMUGI-seitai <99704688+TUMUGI-seitai@users.noreply.github.com>
2022-02-16 08:28:18 +0900

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,19 +1,9 @@
-// new LuminousGallrey(document.querySelectorAll(".gallery"));
-
-// const luminousElem = document.querySelector('.gallery'); 
-// //上記で要素が取得できていれば Luminous を初期化
-// if( luminousElem !== null ) {
-//   new Luminous(luminousElem);
-// }
-
-
-//querySelectorAll() で全ての luminous2 クラスを指定した a 要素を取得
-const luminousElems = document.querySelectorAll('.gallery');
-//取得した要素の数が 0 より大きければ
-if( luminousElems.length > 0 ) {
-  luminousElems.forEach( (elem) => {
-    new Luminous(elem);
-  });
+//querySelectorAll() で全ての luminous3 クラスを指定した a 要素を取得
+const luminousGalleryElems = document.querySelectorAll('.gallery');
+// または getElementsByClassName() を使ってもほぼ同じ（※）
+//const luminousGalleryElems = document.getElementsByClassName('luminous3');
+ //取得した要素の数が 0 より大きければ
+if( luminousGalleryElems.length > 0 ) {
+  // LuminousGallery で初期化
+  new LuminousGallery(luminousGalleryElems);
 }
-
-// console.log(luminousElems);
