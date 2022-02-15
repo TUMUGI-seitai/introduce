b2204def92b900064330e1372ff580ea1f7f5ccf
TUMUGI-seitai <99704688+TUMUGI-seitai@users.noreply.github.com>
2022-02-16 08:30:27 +0900

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,9 +1,1 @@
-//querySelectorAll() で全ての luminous3 クラスを指定した a 要素を取得
-const luminousGalleryElems = document.querySelectorAll('.gallery');
-// または getElementsByClassName() を使ってもほぼ同じ（※）
-//const luminousGalleryElems = document.getElementsByClassName('luminous3');
- //取得した要素の数が 0 より大きければ
-if( luminousGalleryElems.length > 0 ) {
-  // LuminousGallery で初期化
-  new LuminousGallery(luminousGalleryElems);
-}
+new LuminousGallery(document.querySelectorAll(".gallery"));
